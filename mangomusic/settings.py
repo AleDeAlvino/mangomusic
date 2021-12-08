076a40fc9e3c4f9b5907f5a30efcf379a2bca787
Ale De Alvino <alejandra.dealvino.melgoza@gmail.com>
2021-12-08 10:28:44 -0600

    configuracion media 6

--- mangomusic/settings.py
+++ mangomusic/settings.py
@@ -126,7 +126,10 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-DEFAULT_FILE_STORAGE = 'mangomusic.custom_azure.AzureMediaStorage'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+
+DEFAULT_FILE_STORAGE = 'custom_azure.AzureMediaStorage'
 MEDIA_LOCATION = "media"
 AZURE_ACCOUNT_NAME = "mangost"
 AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
